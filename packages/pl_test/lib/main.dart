931de599f3cbade32b1e0dc8b37d5cf702e566f2
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-04-19 19:11:55 +0200

    zoomable graph render in the pl_test

--- packages/pl_test/lib/main.dart
+++ packages/pl_test/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/gestures.dart';
@@ -10,16 +9,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -33,31 +35,46 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("example"),
+        title: const Text("example"),
       ),
-      body: RawGestureDetector(
-        gestures: {
-          TapGestureRecognizer:
-              GestureRecognizerFactoryWithHandlers<TapGestureRecognizer>(
-                  () => TapGestureRecognizer(),
-                  (TapGestureRecognizer instance) {
-            instance.onSecondaryTapUp = (TapUpDetails details) {
-              setState(() {
-                x += 10;
-                y += 10;
-                xOffset -= 20;
-                yOffset -= 20;
-              });
-            };
-          }),
-        },
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: CustomPaint(
-            painter: FaceOutlinePainter(x, y, xOffset, yOffset),
+      body: Stack(
+        children: [
+          LayoutBuilder(
+            builder: (context, constraints) {
+              // print(
+              //   "y: ${this.y} - ${this.y + yOffset} | "
+              //   "x: ${this.x} - ${this.x + xOffset}",
+              // );
+              final size = constraints.biggest;
+              final sizeWidth = size.width;
+              final sizeHeight = size.height;
+              final xSizeRatio = xOffset / sizeWidth;
+              final ySizeRatio = yOffset / sizeHeight;
+              return GestureDetector(
+                onPanUpdate: (details) {
+                  setState(() {
+                    x -= details.delta.dx * xSizeRatio;
+                    y -= details.delta.dy * ySizeRatio;
+                  });
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: CustomPaint(
+                    painter: FaceOutlinePainter(x, y, xOffset, yOffset),
+                  ),
+                ),
+              );
+            },
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("(${x.toStringAsFixed(1)}|${y.toStringAsFixed(1)})"),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -80,19 +97,18 @@ class FaceOutlinePainter extends CustomPainter {
       ..color = Colors.indigo;
 
     final stepSizeX = xOffset / size.width;
+    final stepSizeY = yOffset / size.height;
 
     final offsets = <Offset>[];
     var sizeX = 0.0;
     var functionX = x;
-    final path = Path();
     while (sizeX < size.width) {
-      final y = f(functionX);
+      final rawY = f(functionX);
+      final y = size.height - ((rawY + this.y) / stepSizeY);
       offsets.add(Offset(sizeX, y));
       sizeX++;
       functionX += stepSizeX;
     }
-    // path.close();
-    // canvas.drawPath(path, paint);
     canvas.drawPoints(PointMode.polygon, offsets, paint);
   }
 
