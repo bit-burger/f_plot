bd2e61d0ebe0ab1434f25c7fa9eab10fc51b5601
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-05-26 13:05:26 +0200

    ask before deletion of project from a open project

--- lib/pages/open_project/open_project_app_bar.dart
+++ lib/pages/open_project/open_project_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:f_plot/blocs/open_project/open_project_cubit.dart';
+import 'package:f_plot/widgets/delete_project_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -27,41 +28,46 @@ class _OpenProjectAppBarState extends State<OpenProjectAppBar> {
   @override
   Widget build(BuildContext context) {
     final openProjectCubit = context.read<OpenProjectCubit>();
+    final openProject = openProjectCubit.state.openProject;
     return TextSelectionTheme(
       data: TextSelectionThemeData(
         selectionColor: Colors.white.withAlpha(100),
       ),
-      child: Builder(
-        builder: (context) {
-          return AppBar(
-            centerTitle: true,
-            title: IntrinsicWidth(
-              child: TextField(
-                textAlign: TextAlign.center,
-                controller: _projectNameTextController,
-                onSubmitted: (projectName) {
-                  openProjectCubit.editName(projectName);
-                },
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-                cursorColor: Colors.white,
-                selectionControls: MaterialTextSelectionControls(),
-                decoration: const InputDecoration(
-                  suffixIcon: Icon(Icons.edit, color: Colors.white),
-                  border: InputBorder.none,
-                ),
+      child: Builder(builder: (context) {
+        return AppBar(
+          centerTitle: true,
+          title: IntrinsicWidth(
+            child: TextField(
+              textAlign: TextAlign.center,
+              controller: _projectNameTextController,
+              onSubmitted: (projectName) {
+                openProjectCubit.editName(projectName);
+              },
+              style: const TextStyle(fontSize: 20, color: Colors.white),
+              cursorColor: Colors.white,
+              selectionControls: MaterialTextSelectionControls(),
+              decoration: const InputDecoration(
+                suffixIcon: Icon(Icons.edit, color: Colors.white),
+                border: InputBorder.none,
               ),
             ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  openProjectCubit.deleteProject();
-                },
-              )
-            ],
-          );
-        }
-      ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => DeleteProjectDialog(
+                    projectName: openProject!.name,
+                    onDelete: openProjectCubit.deleteProject,
+                  ),
+                );
+              },
+            )
+          ],
+        );
+      }),
     );
   }
 }
