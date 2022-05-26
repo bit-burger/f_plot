a7a08bae25a11900e25684cd6733bf886121fb2a
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-05-27 00:23:05 +0200

    implement saving and making sure unsaved changes are not discarded

--- lib/pages/open_project/open_project_app_bar.dart
+++ lib/pages/open_project/open_project_app_bar.dart
@@ -10,55 +10,136 @@ class OpenProjectAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
-  @override
-  Widget build(BuildContext context) {
-    final openProjectCubit = context.read<OpenProjectCubit>();
-    return BlocBuilder<OpenProjectCubit, OpenProjectState>(
-      buildWhen: (_, newState) {
-        return newState.projectIsOpen;
-      },
-      builder: (context, state) {
-        final openProject = state.openProject!;
-        return TextSelectionTheme(
-          data: TextSelectionThemeData(
-            selectionColor: Colors.white.withAlpha(100),
+  Widget _buildTitle(BuildContext context) {
+    return Tooltip(
+      message: "edit project name",
+      child: TextButton.icon(
+        onPressed: () => showDialog(
+          useRootNavigator: false,
+          context: context,
+          builder: (_) => const OpenProjectEditNameDialog(),
+        ),
+        icon: const Icon(Icons.edit, color: Colors.white),
+        label: BlocBuilder<OpenProjectCubit, OpenProjectState>(
+          buildWhen: (oldState, newState) =>
+              newState.projectIsOpen &&
+              oldState.openProject?.name != newState.openProject!.name,
+          builder: (context, state) {
+            return Text(state.openProject!.name);
+          },
+        ),
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          textStyle: MaterialStateProperty.all(
+            Theme.of(context).textTheme.headline6,
           ),
-          child: Builder(builder: (context) {
-            return AppBar(
-              centerTitle: true,
-              title: TextButton.icon(
-                onPressed: () => showDialog(
-                  useRootNavigator: false,
-                  context: context,
-                  builder: (_) => const OpenProjectEditNameDialog(),
-                ),
-                icon: const Icon(Icons.edit, color: Colors.white),
-                label: Text(openProject.name),
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(Colors.white),
-                  textStyle: MaterialStateProperty.all(
-                    Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-              ),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (_) => DeleteProjectDialog(
-                        projectName: openProject.name,
-                        onDelete: openProjectCubit.deleteProject,
-                      ),
-                    );
-                  },
-                )
-              ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSaveButton(BuildContext context) {
+    return BlocBuilder<OpenProjectCubit, OpenProjectState>(
+      buildWhen: (oldState, newState) =>
+          oldState.status != newState.status && !newState.openingOrDeleting && newState.projectIsOpen,
+      builder: (context, state) {
+        late final Widget widget;
+        switch (state.status) {
+          case ProjectStatus.saved:
+            widget = const IconButton(
+              key: ValueKey("saved"),
+              tooltip: "saved",
+              onPressed: null,
+              icon: Icon(Icons.save_outlined),
             );
-          }),
+            break;
+          case ProjectStatus.unsaved:
+            widget = IconButton(
+              key: const ValueKey("unsaved"),
+              tooltip: "save",
+              onPressed: context.read<OpenProjectCubit>().saveProject,
+              icon: const Icon(Icons.save),
+            );
+            break;
+          case ProjectStatus.saving:
+            widget = const CircularProgressIndicator.adaptive();
+            break;
+          default:
+            throw StateError("should not be able to reach this point");
+        }
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 250),
+          child: widget,
         );
       },
+    );
+  }
+
+  Widget _buildDeleteButton(BuildContext context) {
+    return IconButton(
+      tooltip: "delete project",
+      icon: const Icon(Icons.delete),
+      onPressed: () {
+        final cubit = context.read<OpenProjectCubit>();
+        showDialog(
+          context: context,
+          builder: (_) => DeleteProjectDialog(
+            projectName: cubit.state.openProject!.name,
+            onDelete: cubit.deleteProject,
+          ),
+        );
+      },
+    );
+  }
+
+  Future<bool> _shouldExitPage(BuildContext context) async {
+    final cubit = context.read<OpenProjectCubit>();
+    if (cubit.state.status ==
+        ProjectStatus.unsaved) {
+      final shouldDiscardResult = await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("do you want to discard unsaved changes?"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, null),
+              child: const Text("cancel"),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, false),
+              child: const Text("save and exit"),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, true),
+              child: const Text("discard"),
+            ),
+          ],
+        ),
+      );
+      switch(shouldDiscardResult) {
+        case true:
+          return true;
+        case false:
+          cubit.saveAndCloseProject();
+          return false;
+        case null:
+          return false;
+      }
+    }
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _shouldExitPage(context),
+      child: AppBar(
+        title: _buildTitle(context),
+        actions: [
+          _buildSaveButton(context),
+          _buildDeleteButton(context),
+        ],
+      ),
     );
   }
 }
--- lib/pages/open_project/open_project_page.dart
+++ lib/pages/open_project/open_project_page.dart
@@ -8,11 +8,41 @@ class OpenProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final openProjectState = context.watch<OpenProjectCubit>().state;
     return Scaffold(
       appBar: const OpenProjectAppBar(),
       body: Center(
-        child: Text(openProjectState.openProject.toString()),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 16),
+            BlocBuilder<OpenProjectCubit, OpenProjectState>(
+              buildWhen: (_, newState) => newState.projectIsOpen,
+              builder: (context, state) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      state.openProject!.name,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      state.openProject!.plotFile,
+                    ),
+                  ],
+                );
+              },
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              child: const Text("edit plot file"),
+              onPressed: () {
+                final cubit = context.read<OpenProjectCubit>();
+                cubit.editPlotfile(cubit.state.openProject!.plotFile + "a");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
