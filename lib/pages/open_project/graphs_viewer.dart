88d1771b3bad19c3a4403b7c6e638775ab844397
bit-burger <65500763+bit-burger@users.noreply.github.com>
2022-05-31 18:48:41 +0200

    improve the default view of the graphs, by adding a controller

--- lib/pages/open_project/graphs_viewer.dart
+++ lib/pages/open_project/graphs_viewer.dart
@@ -4,8 +4,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_nord_theme/flutter_nord_theme.dart';
 import 'package:graph_plotter/graph_plotter.dart';
 
-class GraphsViewer extends StatelessWidget {
+class GraphsViewer extends StatefulWidget {
   const GraphsViewer({super.key});
+
+  @override
+  State<GraphsViewer> createState() => _GraphsViewerState();
+}
+
+class _GraphsViewerState extends State<GraphsViewer> {
+  late final GraphPlotterController _graphPlotterController;
+
+  @override
+  void initState() {
+    super.initState();
+    _graphPlotterController =
+        GraphPlotterController.fromZero(width: 50, height: 50)
+          ..x = -10
+          ..y = -10
+          ..update();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +32,12 @@ class GraphsViewer extends StatelessWidget {
       builder: (context, state) {
         return ClipRect(
           child: GraphPlotter(
+            controller: _graphPlotterController,
             axisWidth: 2.5,
             axisColor: NordColors.$3,
             graphsWidth: 3,
-            axisLabelsTextStyle: const TextStyle(color: NordColors.$3, fontWeight: FontWeight.w500),
+            axisLabelsTextStyle: const TextStyle(
+                color: NordColors.$3, fontWeight: FontWeight.w500),
             graphs: state.functions
                 .where((function) => function.isSingleVariableFunction)
                 .map(
@@ -32,5 +51,11 @@ class GraphsViewer extends StatelessWidget {
         );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _graphPlotterController.dispose();
   }
 }
